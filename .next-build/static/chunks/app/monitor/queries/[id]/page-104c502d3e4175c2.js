0418acce8595d2218750e5ba031353c3ab0c1ed1
sSecOne <sone_ma@163.com>
2026-06-16 10:14:49 +0800

    init project without node_modules

--- .next-build/static/chunks/app/monitor/queries/[id]/page-104c502d3e4175c2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[702],{54644:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,52619,23))}},e=>{e.O(0,[619,441,255,358],()=>e(e.s=54644)),_N_E=e.O()}]);
